af840b588b2b5477bed11366248d7ddbc707de39
I-Alexander-I <109490990+I-Alexander-I@users.noreply.github.com>
2025-12-04 17:02:03 -0600

    slight change

--- client/public/main.js
+++ client/public/main.js
@@ -4,24 +4,25 @@ document.getElementById("fitnessLogBtn").addEventListener("click", fitnessLog);
 document.getElementById("nutritionLogBtn").addEventListener("click", nutritionLog);
 document.getElementById("progressDashboardBtn").addEventListener("click", progressDashboard);
 
+// Fitness/Workout Logging 
 function fitnessLog() {
     Document.getElementbyId("main").innerHTML = "Fitness Log Page";
+
 }
-
-function nutritionLog() {
-    Document.getElementbyId("main").innerHTML = "Nutrition Log Page";
-}
-
-function progressDashboard() {
-    Document.getElementbyId("main").innerHTML = "Progress Dashboard Page";
-}
-
-
-
-// Fitness/Workout Logging 
-
 
 // Meal/Nutrition Logging
+function nutritionLog() {
+    Document.getElementbyId("main").innerHTML = "Nutrition Log Page";
 
+}
 
 // Progress Dashboard & Visualization
+function progressDashboard() {
+    Document.getElementbyId("main").innerHTML = "Progress Dashboard Page";
+    
+}
+
+
+
+
+
