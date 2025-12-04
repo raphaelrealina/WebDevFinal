3718313c38634e7d090f54e8e872bdb25e55bae5
I-Alexander-I <109490990+I-Alexander-I@users.noreply.github.com>
2025-12-04 16:43:19 -0600

    frogot to save

---
+++ client/public/main.js
@@ -0,0 +1,24 @@
+//switching pages
+
+
+function fitnessLog() {
+    Document.getElementbyId("main").innerHTML = "Fitness Log Page";
+}
+
+function nutritionLog() {
+    Document.getElementbyId("main").innerHTML = "Nutrition Log Page";
+}
+
+function progressDashboard() {
+    Document.getElementbyId("main").innerHTML = "Progress Dashboard Page";
+}
+
+
+
+// Fitness/Workout Logging 
+
+
+// Meal/Nutrition Logging
+
+
+// Progress Dashboard & Visualization
